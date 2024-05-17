2c8ef38f0ac5aacf7e731e7bd836a6a19c978ebe
rmfr62 <webshopingolas@gmail.com>
2024-05-17 20:22:46 +0200

    Alap aplikáció

---
+++ src/main/java/hu/gde/runrace/RunRaceApplication.java
@@ -0,0 +1,13 @@
+package hu.gde.runrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunRaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunRaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/gde/runrace/RunRaceApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.gde.runrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunRaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
